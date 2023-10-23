0793739a8834cc3825cfc70552ee23baaa59fc16
mitchmcnaughton <141999182+mitchmcnaughton@users.noreply.github.com>
2023-10-24 02:45:22 +1100

    rewrote the original readme markdown creator and added the function for the license badge + made a readme with the index.js

---
+++ README.md
@@ -0,0 +1,30 @@
+# readme-creator
+
+## Description
+     a cli that uses prompts to generate a readme file
+        
+## Table of Contents ![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+        
+## Installation
+     download from github
+        
+## Usage
+    make sure you are in the directory, type "node index.js" in the terminal and fill in the prompts
+        
+## Contributing
+    just me :)
+        
+## Tests
+    undefined
+    
+## License
+    MIT
+        
+## Questions
+    If you have any questions, please feel free to reach out to me at [mitch.mcnaughton3@gmail.com](mailto:mitch.mcnaughton3@gmail.com). You can also find more of my work on [GitHub](https://github.com/undefined).
--- index.js
+++ index.js
@@ -1,158 +1,144 @@
-// TODO: Include packages needed for this application
-const fs = require('fs')
-const inquirer = require('inquirer')
-// TODO: Create an array of questions for user input
-async function collectUserInput(){
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Map of license keywords to their corresponding badges
+const licenseBadges = {
+    'AFL-3.0': '![AFL-3.0](https://img.shields.io/badge/License-AFL--3.0-blue.svg)',
+    'Apache-2.0': '![Apache-2.0](https://img.shields.io/badge/License-Apache--2.0-blue.svg)',
+    'Artistic-2.0': '![Artistic-2.0](https://img.shields.io/badge/License-Artistic--2.0-blue.svg)',
+    'BSL-1.0': '![BSL-1.0](https://img.shields.io/badge/License-BSL--1.0-blue.svg)',
+    'BSD-2-Clause': '![BSD-2-Clause](https://img.shields.io/badge/License-BSD--2--Clause-blue.svg)',
+    'BSD-3-Clause': '![BSD-3-Clause](https://img.shields.io/badge/License-BSD--3--Clause-blue.svg)',
+    'BSD-3-Clause-Clear': '![BSD-3-Clause-Clear](https://img.shields.io/badge/License-BSD--3--Clause--Clear-blue.svg)',
+    'BSD-4-Clause': '![BSD-4-Clause](https://img.shields.io/badge/License-BSD--4--Clause-blue.svg)',
+    '0BSD': '![0BSD](https://img.shields.io/badge/License-0BSD-blue.svg)',
+    'CC': '![CC](https://img.shields.io/badge/License-CC-blue.svg)',
+    'CC0-1.0': '[![CC0-1.0](https://img.shields.io/badge/License-CC0--1.0-blue.svg)',
+    'CC-BY-4.0': '![CC-BY-4.0](https://img.shields.io/badge/License-CC--BY--4.0-blue.svg)',
+    'CC-BY-SA-4.0': '![CC-BY-SA-4.0](https://img.shields.io/badge/License-CC--BY--SA--4.0-blue.svg)',
+    'WTFPL': '![WTFPL](https://img.shields.io/badge/License-WTFPL-blue.svg)',
+    'ECL-2.0': '![ECL-2.0](https://img.shields.io/badge/License-ECL--2.0-blue.svg)',
+    'EPL-1.0': '![ EPL-1.0](https://img.shields.io/badge/License-EPL--1.0-blue.svg)',
+    'EPL-2.0': '![EPL-2.0](https://img.shields.io/badge/lLicense-EPL--2.0-blue.svg)',
+    'EUPL-1.1': '![EUPL-1.1](https://img.shields.io/badge/License-EUPL--1.1-blue.svg)',
+    'AGPL-3.0': '![AGPL-3.0](https://img.shields.io/badge/License-AGPL--3.0-blue.svg)',
+    'GPL': '![GPL](https://img.shields.io/badge/License-GPL-blue.svg)',
+    'GPL-2.0': '![GPL-2.0](https://img.shields.io/badge/License-GPL--2.0-blue.svg)',
+    'GPL-3.0': '![GPL-3.0](https://img.shields.io/badge/License-GPL--3.0-blue.svg)',
+    'LGPL': '![LGPL](https://img.shields.io/badge/License-LGPL-blue.svg)',
+    'LGPL-2.1': '![LGPL-2.1](https://img.shields.io/badge/License-LGPL--2.1-blue.svg)',
+    'LGPL-3.0': '![LGPL-3.0](https://img.shields.io/badge/License-LGPL--3.0-blue.svg)',
+    'ISC': '![ISC](https://img.shields.io/badge/License-ISC-blue.svg)',
+    'LPPL-1.3c': '![LPPL-1.3c](https://img.shields.io/badge/License-LPPL--1.3c-blue.svg)',
+    'MS-PL': '![MS-PL](https://img.shields.io/badge/License-MS--PL-blue.svg)',
+    'MIT': '![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'MPL-2.0': '![MPL-2.0](https://img.shields.io/badge/License-MPL--2.0-blue.svg)',
+    'OSL-3.0': '![OSL-3.0](https://img.shields.io/badge/License-OSL--3.0-blue.svg)',
+    'PostgreSQL': '![PostgreSQL](https://img.shields.io/badge/License-PostgreSQL-blue.svg)',
+    'OFL-1.1': '![OFL-1.1](https://img.shields.io/badge/License-OFL--1.1-blue.svg)',
+    'NCSA': '![NCSA](https://img.shields.io/badge/License-NCSA-blue.svg)',
+    'Unlicense': '![Unlicense](https://img.shields.io/badge/License-Unlicense-blue.svg)',
+    'Zlib': '![Zlib](https://img.shields.io/badge/License-Zlib-blue.svg)',
+    'No License': '', // Empty string for "No License"
+    };
+
+async function collectUserInput() {
+  try {
     const questions = [{
-        type: 'input',
-        name: 'title',
-        message: 'Enter your repository title:',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter your  application repository description:',
-    },
-    {
-        type: 'input',
-        name: 'installation instructions',
-        message: 'Enter your installation instructions for your application:',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter a short description about the usage of your application?',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Enter who contributed to the github project:',
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Enter your github username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email?',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Enter test instructions for your application?',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'Enter the license you want to use?',
-    },
-
-
-
+      type: 'input',
+      name: 'title',
+      message: 'Enter your repository title:',
+  },
+  {
+      type: 'input',
+      name: 'description',
+      message: 'Enter your  application repository description:',
+  },
+  {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter your installation instructions for your application:',
+  },
+  {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter a short description about the usage of your application?',
+  },
+  {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter who contributed to the github project:',
+  },
+  {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your github username?',
+  },
+  {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email?',
+  },
+  {
+      type: 'input',
+      name: 'test',
+      message: 'Enter test instructions for your application?',
+  },
+  {
+      type: 'list',
+      name: 'license',
+      message: 'Select a license for your project:',
+      choices: Object.keys(licenseBadges),
+      
+  },
+      
     ];
-    
-    return await inquirer.prompt(questions);
-}
 
+    const answers = await inquirer.prompt(questions);
 
-// Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+    // Get the license badge for the selected license
+    const selectedLicense = answers.license;
+    const licenseBadge = licenseBadges[selectedLicense];
 
-function renderLicenseBadge(license) {
-  if (!license) {
-    return '';
-  }
-
-  const licenseBadges = {
-    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-    Apache: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)',
-    GPL: '[![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-    
-    // Add more licenses and their badge URLs as needed
-  };
-
-  return licenseBadges[license] || '';
-}
-
-// Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (!license) {
-    return '';
-  }
-
-  const licenseLinks = {
-    MIT: 'https://opensource.org/licenses/MIT',
-    Apache: 'https://www.apache.org/licenses/LICENSE-2.0',
-    GPL: 'https://www.gnu.org/licenses/gpl-3.0',
-    // Add more licenses and their links as needed
-  };
-
-  return licenseLinks[license] || '';
-}
-
-// Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return '';
-  }
-
-    const licenseText =  `
-## License
-${renderLicenseBadge(license)}
-This project is licensed under the [${license} License](${renderLicenseLink(license)}).
-  `;
-}
-
-
-
-// TODO: Create a function to write README file
-function writeToFile(data) {
-return `# ${data.title}
+    // Generate the README content
+    const readmeContent = 
+    `# ${answers.title}
 
 ## Description
-    ${data.description}
-    
-## Table of Contents
+     ${answers.description}
+        
+## Table of Contents ${licenseBadge}
     - [Installation](#installation)
     - [Usage](#usage)
     - [License](#license)
     - [Contributing](#contributing)
     - [Tests](#tests)
     - [Questions](#questions)
-    
+        
 ## Installation
-    ${data.installation}
-    
+     ${answers.installation}
+        
 ## Usage
-    ${data.usage}
-    
+    ${answers.usage}
+        
 ## Contributing
-    ${data.contributing}
-    
+    ${answers.contribution}
+        
 ## Tests
-    ${data.tests}
+    ${answers.tests}
     
+## License
+    ${answers.license}
+        
 ## Questions
-    If you have any questions, please feel free to reach out to me at [${data.email}](mailto:${data.email}). You can also find more of my work on [GitHub](https://github.com/${data.github}).`
+    If you have any questions, please feel free to reach out to me at [${answers.email}](mailto:${answers.email}). You can also find more of my work on [GitHub](https://github.com/${answers.github}).`;
+
+    // Write the README to a new file
+    fs.writeFileSync('README.md', readmeContent, 'utf8');
+    console.log('README.md has been generated.');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 }
 
-// TODO: Create a function to initialize app
- async function init() {
-    try {
-        const userData = await collectUserInput();
-        const readmeContent= writeToFile(userData);
-        
-        fs.writeFileSync('README.md', readmeContent)
-        console.log('README file has been generated successfully!');
-        
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-
-init();
+collectUserInput();
